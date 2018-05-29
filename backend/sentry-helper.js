e9f8e0405e3fab6024bb62bb34c917701b73f3b7
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-05-29 08:13:16 -0400

    adding sample sentry api template

---
+++ backend/sentry-helper.js
@@ -0,0 +1,25 @@
+const Sentry = require('sentry-api').Client;
+
+// Create a new client using your Sentry DSN.
+const sentry = new Sentry('https://abc123:@app.getsentry.com');
+
+// // If you're using authentication tokens, you'll want to pass the token as
+// // a configuration option.
+// var sentry = new Sentry('https://abc123:@app.getsentry.com', {
+//   token: 'mytoken'
+// });
+
+// // You can also leave off the DSN if you're using token authentication and
+// // hosted sentry.
+// var sentry = new Sentry({
+//   token: 'mytoken'
+// });
+
+// Retrieve a project using the promise style.
+sentry.projects.get('org-slug', 'project-slug')
+  .then((project) => {
+    console.log(project.name);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
